4217aeef7f4a82229ae47474862f36689ae30141
nptuyenn <166326602+nptuyenn@users.noreply.github.com>
2024-12-23 00:03:58 +0700

    ADD lab5 bai 2

---
+++ Lab05/Bai2/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Bai2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Emailtxt = new System.Windows.Forms.TextBox();
+            this.Passwordtxt = new System.Windows.Forms.TextBox();
+            this.LogInbtn = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblRecent = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Email = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.From = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Time = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password: ";
+            // 
+            // Emailtxt
+            // 
+            this.Emailtxt.Location = new System.Drawing.Point(113, 9);
+            this.Emailtxt.Name = "Emailtxt";
+            this.Emailtxt.Size = new System.Drawing.Size(392, 26);
+            this.Emailtxt.TabIndex = 2;
+            // 
+            // Passwordtxt
+            // 
+            this.Passwordtxt.Location = new System.Drawing.Point(113, 46);
+            this.Passwordtxt.Name = "Passwordtxt";
+            this.Passwordtxt.Size = new System.Drawing.Size(392, 26);
+            this.Passwordtxt.TabIndex = 3;
+            this.Passwordtxt.UseSystemPasswordChar = true;
+            // 
+            // LogInbtn
+            // 
+            this.LogInbtn.Location = new System.Drawing.Point(766, 21);
+            this.LogInbtn.Name = "LogInbtn";
+            this.LogInbtn.Size = new System.Drawing.Size(149, 51);
+            this.LogInbtn.TabIndex = 4;
+            this.LogInbtn.Text = "Log In";
+            this.LogInbtn.UseVisualStyleBackColor = true;
+            this.LogInbtn.Click += new System.EventHandler(this.LogInbtn_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(12, 82);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(52, 20);
+            this.lblTotal.TabIndex = 5;
+            this.lblTotal.Text = "Total: ";
+            // 
+            // lblRecent
+            // 
+            this.lblRecent.AutoSize = true;
+            this.lblRecent.Location = new System.Drawing.Point(225, 82);
+            this.lblRecent.Name = "lblRecent";
+            this.lblRecent.Size = new System.Drawing.Size(69, 20);
+            this.lblRecent.TabIndex = 7;
+            this.lblRecent.Text = "Recent: ";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Email,
+            this.From,
+            this.Time});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(16, 125);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(956, 313);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Email
+            // 
+            this.Email.Text = "Email";
+            this.Email.Width = 300;
+            // 
+            // From
+            // 
+            this.From.Text = "From";
+            this.From.Width = 200;
+            // 
+            // Time
+            // 
+            this.Time.Text = "Time";
+            this.Time.Width = 150;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(541, 49);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(147, 24);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Show password";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.lblRecent);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.LogInbtn);
+            this.Controls.Add(this.Passwordtxt);
+            this.Controls.Add(this.Emailtxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Emailtxt;
+        private System.Windows.Forms.TextBox Passwordtxt;
+        private System.Windows.Forms.Button LogInbtn;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblRecent;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Email;
+        private System.Windows.Forms.ColumnHeader From;
+        private System.Windows.Forms.ColumnHeader Time;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Lab05/Bai2/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MimeKit;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using MailKit;
+namespace Bai2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void LogInbtn_Click(object sender, EventArgs e)
+        {
+            LogInbtn.Enabled = false; // Vô hiệu hóa nút LOGIN trong khi tải email
+            try
+            {
+                await Task.Run(() =>
+                {
+                    using (var client = new ImapClient())
+                    {
+                        // Kết nối đến máy chủ IMAP
+                        client.Connect("imap.gmail.com", 993, true);
+
+                        // Xác thực
+                        client.Authenticate(Emailtxt.Text, Passwordtxt.Text);
+
+                        // Truy cập hộp thư "INBOX"
+                        var inbox = client.Inbox;
+                        inbox.Open(MailKit.FolderAccess.ReadOnly);
+
+                        // Cập nhật thông tin Total và Recent
+                        Invoke(new Action(() =>
+                        {
+                            lblTotal.Text = "Total: " + inbox.Count.ToString();
+                            int recentCount = Math.Min(inbox.Count, 10);
+                            lblRecent.Text = "Recent: " + recentCount.ToString();
+
+                            // Xóa dữ liệu cũ trong ListView
+                            listView1.Items.Clear();
+                        }));
+
+                        // Lấy và hiển thị email
+                        for (int i = inbox.Count - 1; i >= inbox.Count - Math.Min(inbox.Count, 10); i--)
+                        {
+                            var message = inbox.GetMessage(i);
+
+                            // Thêm email vào ListView trên UI thread
+                            Invoke(new Action(() =>
+                            {
+                                var item = new ListViewItem(new[]
+                                {
+                            message.Subject ?? "(No Subject)",
+                            message.From.ToString(),
+                            message.Date.ToString("yyyy-MM-dd HH:mm:ss")
+                        });
+                                listView1.Items.Add(item);
+                            }));
+                        }
+
+                        // Ngắt kết nối
+                        client.Disconnect(true);
+                    }
+                });
+
+                MessageBox.Show("Email loads successfully!", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                LogInbtn.Enabled = true; // Kích hoạt lại nút LOGIN
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                // Hiển thị mật khẩu
+                Passwordtxt.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                // Ẩn mật khẩu
+                Passwordtxt.UseSystemPasswordChar = true;
+            }
+        }
+    }
+}
